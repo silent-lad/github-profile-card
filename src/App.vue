3a93229a93f0d1faecf6db9a1e8c0aa28ef979b7
Divyansh Tripathi <divyanshtripathi1998@gmail.com>
2021-10-30 03:32:37 +0530

    Checkpoint border

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,4 @@ export default {
 body {
   background: #2c3e50;
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  
-}
 </style>
